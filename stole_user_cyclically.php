d5f60facc18f84bda4eb98b367f4b4452a6f6c7a
hhqcontinue <hhqcontinue@gmail.com>
2015-09-08 18:01:35 +0800

    update

--- stole_user_cyclically.php
+++ stole_user_cyclically.php
@@ -3,12 +3,13 @@
  * @Author: huhuaquan
  * @Date:   2015-08-26 11:38:18
  * @Last Modified by:   huhuaquan
- * @Last Modified time: 2015-08-26 11:43:46
+ * @Last Modified time: 2015-09-08 17:30:55
  */
 require_once './spider/user.php';
 require_once './function.php';
 require_once './spider/curl.php';
 require_once './spider/pdo_mysql.php';
+require_once './spider/predis.php';
 $user_cookie = '_za=a41e1b8b-517a-4fea-9465-88e8c80ba17e;q_cl=3198dbc291fa40d7b717f9a4dd5ec90e|1439792872000|1439792872000;_xsrf=981ffd949fbc70e73cc4bb2559243ac8;cap_id="YmViMDk0YTdjMjUyNDc4MjhmOWU5MDkyMTg3NWRlNGY=|1439792872|7eb10c44aead609ab6e63f3eb2b5856149076942";z_c0="QUFEQTRZbzZBQUFYQUFBQVlRSlZUZjhMLVZYNnBhUDBYYzJIOFJtUGs2aFlianFRU3NRR3hRPT0=|1439792895|4f033f6e2f99a39b152a59c32496dfc954cbe6fd";__utma=51854390.888606616.1439792875.1439792875.1439891906.2;__utmb=51854390.2.10.1439891906;__utmc=51854390;__utmt=1;__utmv=51854390.100-1|2=registration_date=20141017=1^3=entry_date=20141017=1__utmz=51854390.1439891906.2.2.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided)';
 
 $request_queue = array('mora-hu');
