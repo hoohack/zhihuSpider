6f8450ed51505bee08941e03d25a0c516c5e3263
hhqcontinue <hhqcontinue@gmail.com>
2015-09-09 14:34:05 +0800

    add redis support

--- stole_user_cyclically.php
+++ stole_user_cyclically.php
@@ -3,7 +3,7 @@
  * @Author: huhuaquan
  * @Date:   2015-08-26 11:38:18
  * @Last Modified by:   huhuaquan
- * @Last Modified time: 2015-09-08 17:30:55
+ * @Last Modified time: 2015-09-09 14:33:49
  */
 require_once './spider/user.php';
 require_once './function.php';
@@ -12,43 +12,79 @@ require_once './spider/pdo_mysql.php';
 require_once './spider/predis.php';
 $user_cookie = '_za=a41e1b8b-517a-4fea-9465-88e8c80ba17e;q_cl=3198dbc291fa40d7b717f9a4dd5ec90e|1439792872000|1439792872000;_xsrf=981ffd949fbc70e73cc4bb2559243ac8;cap_id="YmViMDk0YTdjMjUyNDc4MjhmOWU5MDkyMTg3NWRlNGY=|1439792872|7eb10c44aead609ab6e63f3eb2b5856149076942";z_c0="QUFEQTRZbzZBQUFYQUFBQVlRSlZUZjhMLVZYNnBhUDBYYzJIOFJtUGs2aFlianFRU3NRR3hRPT0=|1439792895|4f033f6e2f99a39b152a59c32496dfc954cbe6fd";__utma=51854390.888606616.1439792875.1439792875.1439891906.2;__utmb=51854390.2.10.1439891906;__utmc=51854390;__utmt=1;__utmv=51854390.100-1|2=registration_date=20141017=1^3=entry_date=20141017=1__utmz=51854390.1439891906.2.2.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided)';
 
-$request_queue = array('mora-hu');
-
+//redis instance
+$redis = PRedis::getInstance();
+$redis->flushdb();
+$redis->lpush('request_queue', 'mora-hu');
+$max_connect = 200;
 while (1)
 {
 	echo "begin get user info...\n";
-	if (empty($request_queue))
+	$total = $redis->llen('request_queue');
+	if ($total == 0)
 	{
+		echo "-----done...-----\n";
 		break;
 	}
-	$u_id = array_shift($request_queue);
-	$current_user = array();
-	$result = Curl::request('GET', 'http://www.zhihu.com/people/' . $u_id . '/followees');
-	$params = array(
-		'where' => array(
-			'u_id' => $u_id
-		)
-	);
-
-	if (!User::existed($params, 'user'))
+	else if ($total <= $max_connect)
 	{
-		$current_user = getUserInfo($result);
-		User::add($current_user);
+		$current_count = $total;
 	}
-
-	$user_info = User::info($u_id);
-	$user_followees_count = User::getFollowCount($u_id);
-
-	if ($user_info['followees_count'] != $user_followees_count)
+	else
 	{
-		$followee_users = getUserList($result, $u_id, 'followees', $user_info['followees_count']);
-		foreach ($followee_users as $user)
+		$current_count = $max_connect;
+	}
+	for ($i = 1; $i <= $current_count; ++$i)
+	{
+		$pid = pcntl_fork();
+		if ($pid == -1)
 		{
-			array_push($request_queue, $user[2]);
+			echo "-----fork child process failed...-----\n";
+			exit(0);
 		}
-		User::addFollowList($followee_users);
-	}
+		if (!$pid)
+		{
+			$tmp_redis = PRedis::getInstance();
+			$tmp_u_id = $tmp_redis->lpop('request_queue');
+			echo "getting {$tmp_u_id} follower list\n";
+			$result = Curl::request('GET', 'http://www.zhihu.com/people/' . $tmp_u_id . '/followees');
+			$params = array(
+				'where' => array(
+					'u_id' => $tmp_u_id
+				)
+			);
+			if (!User::existed($params, 'user'))
+			{
+				$current_user = getUserInfo($result);
+				User::add($current_user);
+			}
 
-	usleep(100);
-	echo "add new " . $user_info['followees_count'] . " users\n";
+			$user_info = User::info($tmp_u_id);
+			$user_followees_count = User::getFollowCount($tmp_u_id);
+
+			if ($user_info['followees_count'] != $user_followees_count)
+			{
+				$followee_users = getUserList($result, $tmp_u_id, 'followees', $user_info['followees_count']);
+				foreach ($followee_users as $user)
+				{
+					$tmp_redis->lpush('request_queue', $user[2]);
+				}
+				User::addFollowList($followee_users);
+			}
+			echo "add new " . $user_info['followees_count'] . " users\n";
+			$tmp_redis->close();
+			exit($i);
+		}
+		usleep(1);
+	}
+	while (pcntl_waitpid(0, $status) != -1)
+	{
+		$status = pcntl_wexitstatus($status);
+		if (pcntl_wifexited($status))
+		{
+			echo "yes";
+		}
+		echo "$status finished\n";
+	}
+	exit;
 }
